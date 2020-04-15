a72061edec413ecedf141e12838d98360620fbef
Angelo de Salves <angelopeto@yahoo.com.br>
2020-04-14 23:48:47 -0300

    Modulo 01: react - configurando o ambiente com babel/webpack

---
+++ modulo01/react/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //  Conjunto de configurações criadas por terceiros
+    presets: [
+        //  Converte o código js de acordo com o ambiente da aplicação
+        '@babel/preset-env',
+        //  Adiciona as funcionalidades do react na converção acima
+        '@babel/preset-react'
+    ]
+}
